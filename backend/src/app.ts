77d0b56e190bdc9db1f6af661fa27b89712effb3
Rafael Tomazini <rafael.tomazini@bankeiro.com.br>
2025-12-11 09:35:30 -0300

    feat: inital route setup

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error("Error starting the server:", error);
+  } else {
+    console.log(`Server is running at http://localhost:${port}`);
+  }
+});
---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/login", (req, res) => {});
+router.post("/register", (req, res) => {});
+router.post("/logout", (req, res) => {});
+
+export default router;
